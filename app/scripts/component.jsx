eca65170baf5731707399153fdc4dadff7196e13
Usman <uafzal@living-group.com>
2016-03-07 17:15:44 +0000

    initial commit

---
+++ app/scripts/component.jsx
@@ -0,0 +1,9 @@
+const Layout = React.createClass({
+  render: function() {
+    return (
+      <h1>Hello World</h1>
+    )
+  }
+});
+
+ReactDOM.render(<Layout/>, document.getElementById('title'));
